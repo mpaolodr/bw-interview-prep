cfffb9d5e994f649d8431d06710ecfc1e35a7db7
mpaolodr <mpaolodr_1504@yahoo.com>
2020-06-23 23:05:04 -0400

    added comments on two_sum.py

--- day2/two_sum.py
+++ day2/two_sum.py
@@ -5,11 +5,17 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+
+        # we create a dictionary from each element in nums with keys being the element and value to be index
         num_keys = {val: index for index, val in enumerate(nums)}
 
+        # we loop through nums array and check if target - current num is in dictionary
         for index in range(len(nums)):
 
             value_wanted = target - nums[index]
 
+            # if value wanted is in dictionary and it's not value at given index
             if value_wanted in num_keys and index != num_keys[value_wanted]:
+
+                # output asks for list with index of values that have sum of target
                 return [index, num_keys[value_wanted]]
